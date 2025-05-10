cd89f09ec291e206e2b9924850346b3ed4493967
adenon-amr <adenon.amr@gmail.com>
2025-05-10 15:35:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 EMAIL = os.environ["EMAIL"]
 PASSWORD = os.environ["PASSWORD"]
 GITHUB_PAT = os.environ["TOKEN"]
+GEMINI_API_KEY = os.environ["GEMINI_API_KEY"]
 
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
